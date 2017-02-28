df969d9f4bff3562276df048e676ee553831ba21
zhanguang <zhanguang@ykxing.com>
2017-02-28 18:19:22 +0800

    增加沙箱配置；

--- example/alipayConfig.php
+++ example/alipayConfig.php
@@ -8,8 +8,15 @@
 return [
 //    'notifyUrl' => 'https://local.ykxng.com/zhan/zhan/example/notify.php',
     'notifyUrl' => 'https://local.ykxing.com/zhan/yiyuan/AliPay_notify',
-    'rsaPrivateKey' => file_get_contents(__DIR__ . '/key/rsa_private_key'),
-    'rsaAliPubPath' => file_get_contents(__DIR__ . '/key/ali_pay_rsa_public_key'),
+
     'signType' => 'RSA2',
-    'appId' => '2017021705720667'
+//    'appId' => '2017021705720667'
+//    'rsaPrivateKey' => file_get_contents(__DIR__ . '/key/rsa_private_key'),
+//    'rsaAliPubPath' => file_get_contents(__DIR__ . '/key/ali_pay_rsa_public_key'),
+
+    // 沙箱配置
+    'appId' => '2016080100139922',
+    'getewayUrl' => 'https://openapi.alipaydev.com/gateway.do',
+    'rsaPrivateKey' => file_get_contents(__DIR__ . '/sandboxKey/rsa_private_key'),
+    'rsaAliPubPath' => file_get_contents(__DIR__ . '/sandboxKey/ali_pay_rsa_public_key'),
 ];
